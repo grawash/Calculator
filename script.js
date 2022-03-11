a48e45610c4bc99e0fdbf53810b93103e615ad25
zurbash <duta1101@bk.ru>
2022-03-11 17:41:15 +0400

    added operate function which takes an operator and 2 numbers and calls correct function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function operate(operator,a,b){
+    if(operator==="+") console.log(addNum(a,b));
+    else if(operator === "-") subtract(a,b);
+    else if(operator === "*") multiply(a,b);
+    else divide(a,b);
+
+}
 function addNum(a, b){
     return a+b;
 }
