7a54d2d8a8366158af0988c49acf6ac9fe8ae8c0
zurbash <duta1101@bk.ru>
2022-03-11 19:09:48 +0400

    added functions which display value of buttons, added variables for storing values, added function to refresh and display correct calculations

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const numButtons = document.querySelectorAll(".num");
+const operatorButtons = document.querySelectorAll(".special");
+let display = document.querySelector(".display");
+let displayValue;
+let storeValue;
+let operSign;
+//adds displayShow function to each number button
+numButtons.forEach((button) =>{
+    button.addEventListener('click', () => displayShow(button.textContent));
+});
+//assigns doOperation function to each operator button
+operatorButtons.forEach((button) =>{
+    button.addEventListener('click', () => doOperation(button.textContent));
+});
+//stores previous displayValue to storeValue, assigns operator to operSign, if operator === "=" calls equal function
+function doOperation(operator){
+    if(operator==="=") equal(operSign,storeValue,displayValue);
+    else{
+    storeValue=displayValue;
+    operSign=operator;
+    display.textContent=operator;
+    }
+}
+//takes operator and two values, returns calculated value
+function equal(sign,a,b){
+    let z=Math.floor(a);
+    let x=Math.floor(b);
+    console.log(z);
+    console.log(x);
+    if(sign==="+"){
+    display.textContent=(z+x);
+    }
+    else if(sign==="-"){
+        display.textContent=(z-x);
+    }
+    else if(sign==="*"){
+        display.textContent=(z*x);
+    }
+    else if(sign==="/"){
+        display.textContent=(z/x);
+    }
+}
+//math operations
 function operate(operator,a,b){
     if(operator==="+") console.log(addNum(a,b));
     else if(operator === "-") subtract(a,b);
@@ -16,4 +59,12 @@ function multiply(a, b){
 }
 function divide(a, b){
     return a/b;
+}
+//displays numbers when clicked on number buttons, also stores value in displayValue
+function displayShow(smth){
+    if(display.textContent==="+" || display.textContent==="-" || display.textContent==="*" || display.textContent==="/"){
+        display.textContent="";
+    }
+    display.textContent+=smth;
+    displayValue = display.textContent;
 }
